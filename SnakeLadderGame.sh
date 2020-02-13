4c008bfa568c822292b38e76a70a730153090d2e
Kuldeep8853 <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-13 19:11:22 +0530

    [Kuldeep]Add.Play the game with 2 player and report which player won the game

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -90,19 +90,24 @@ Snake(){
 	esac
 }
 playerOne=0
-count=0
+playerTwo=0
+count1=0
+count2=0
 playGame(){
+	playerChange=1
 	while(( playerOne<100 ))
 	do
-		play=$(( $(( $RANDOM%6 ))+1 ))
-		count=$(( $count+1 ))
-		echo "$count time roll the die to get the $play"
-		playerOne=$(( $playerOne+$play ))
+	if(( $playerChange==1 ))
+	then
+		play1=$(( $(( $RANDOM%6 ))+1 ))
+		count1=$(( $count1+1 ))
+		echo "$count1 time roll the die to get the $play1"
+		playerOne=$(( $playerOne+$play1 ))
 		#ladder
 			ladder1=$( Ladder $playerOne )
 			if(( $ladder1 >= 1 ))
 			then
-				echo "player score is $playerOne"
+				echo "First player score is $playerOne"
 				playerOne=$(( $playerOne+$ladder1 ))
 				echo "++> with help of ladder hike $ladder1 "
 			fi
@@ -110,23 +115,62 @@ playGame(){
 			snake1=$( Snake $playerOne )
 			if(( $snake1 >= 1 ))
 			then
-				echo "player score is $playerOne"
+				echo "First player score is $playerOne"
 				playerOne=$(( $playerOne-$snake1 ))
 				echo "--> with help of snake minus $snake1 the total score "
 			fi
 		if(( $playerOne>100 ))
 		then
-			playerOne=$(( $playerOne-$play ))
-			echo "Player score is $playerOne "
+			playerOne=$(( $playerOne-$play1 ))
+			echo "First Player score is $playerOne "
 		else
-			echo "player score is $playerOne "
+			echo "First player score is $playerOne "
 		fi
 	#CheckWinner
 		if(( $playerOne == 100 ))
 		then
-			echo "Win the player first"
+			echo "Win the first player"
+			break
 		fi
+	playerChange=2
+	fi
+	if(( $playerChange==2 ))
+	then
+		play2=$(( $(( $RANDOM%6 ))+1 ))
+		count2=$(( $count2+1 ))
+		echo "$count2 time roll the die to get the $play2"
+		playerTwo=$(( $playerTwo+$play2 ))
+		#ladder
+			ladder1=$( Ladder $playerTwo )
+			if(( $ladder1 >= 1 ))
+			then
+				echo "Second player score is $playerTwo"
+				playerTwo=$(( $playerTwo+$ladder1 ))
+				echo "++> with help of ladder hike $ladder1 "
+			fi
+		#Snake
+			snake1=$( Snake $playerTwo )
+			if(( $snake1 >= 1 ))
+			then
+				echo "Second player score is $playerTwo"
+				playerTwo=$(( $playerTwo-$snake1 ))
+				echo "--> with help of snake minus $snake1 the total score "
+			fi
+		if(( $playerTwo>100 ))
+		then
+			playerTwo=$(( $playerTwo-$play2 ))
+			echo "Second Player score is $playerTwo "
+		else
+			echo "Second player score is $playerTwo "
+		fi
+	#CheckWinner
+		if(( $playerTwo == 100 ))
+		then
+			echo "Win the second player "
+			break
+		fi
+	playerChange=1
+	fi
 	done
 }
 playGame
-echo "$count time Rolls the die and win the game"
