256736363977e370bbd618463354a1935c438ddd
Kuldeep8853 <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-13 16:17:22 +0530

    [Kuldeep]Add.Single player at start position 0

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,2 +1,39 @@
 #!/bin/bash/
-echo "Start the snake and ladder game, and Position of player zero"
+declare -A GameBoard[10,10]
+initializeGameBoard(){
+	number=100
+	for(( i=1;i<=10;i++ ))
+	do
+		if (( i%2 != 0 ))
+		then
+			for(( j=1;j<=10;j++ ))
+			do
+				GameBoard[$i,$j]=$number
+				number=$(( $number-1 ))
+			done
+		else
+			for(( j=10;j>0;j-- ))
+			do
+				GameBoard[$i,$j]=$number
+				number=$(( $number-1 ))
+			done
+		fi
+done
+}
+initializeGameBoard
+
+PrintGameBoard(){
+	echo "=============================================================="
+	for(( i=1;i<=10;i++ ))
+	do
+		for(( j=1;j<=10;j++ ))
+		do
+			value=$(printf "%03d" ${GameBoard[$i,$j]})
+			printf "| $value "
+		done
+		echo " |"
+		echo "=============================================================="
+	done
+}
+PrintGameBoard
+echo "Player Position is zero"
