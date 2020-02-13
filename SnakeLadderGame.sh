c4fb2d68b77a55492a8ab6e73f5160a239c74465
Kuldeep8853 <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-13 18:24:19 +0530

    [Kuldeep]Add.the player then check for option Ladder and Snake

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,13 +1,117 @@
 #!/bin/bash/
+
+Ladder(){
+	case $1 in
+	3)
+		ladder= 58
+		echo $ladder
+	;;
+	6)
+		ladder=21
+		echo $ladder
+	;;
+	20)
+		ladder=50
+		echo $ladder
+	;;
+	28)
+		ladder=49
+		echo $ladder
+	;;
+	36)
+		ladder=19
+		echo $ladder
+	;;
+	54)
+		ladder=15
+		echo $ladder
+	;;
+	63)
+		ladder=32
+		echo $ladder
+	;;
+	70)
+		ladder=20
+		echo $ladder
+	;;
+	87)
+		ladder=6
+		echo $ladder
+	;;
+	*)
+		ladder=0
+		echo $ladder
+	;;
+	esac
+}
+
+Snake(){
+	case $1 in
+	27)
+		snake= 20
+		echo $snake
+	;;
+	35)
+		snake=30
+		echo $snake
+	;;
+	37)
+		snake=33
+		echo $snake
+	;;
+	66)
+		snake=42
+		echo $snake
+	;;
+	50)
+		snake=16
+		echo $snake
+	;;
+	59)
+		snake=13
+		echo $snake
+	;;
+	76)
+		snake=13
+		echo $snake
+	;;
+	89)
+		snake=22
+		echo $snake
+	;;
+	98)
+		snake=90
+		echo $snake
+	;;
+	*)
+		snake=0
+		echo $snake
+	;;
+	esac
+}
 playerOne=0
 count=0
 playGame(){
 	while(( playerOne<100 ))
 	do
 		play=$(( $(( $RANDOM%6 ))+1 ))
-		playerOne=$(( $playerOne+$play ))
 		count=$(( $count+1 ))
 		echo "$count time roll the die to get the $play"
+		playerOne=$(( $playerOne+$play ))
+		#ladder
+			ladder1=$( Ladder $playerOne )
+			if(( $ladder1 > 0 ))
+			then
+				playerOne=$(( $playerOne+$ladder1 ))
+				echo "++> with help of ladder hike $ladder1 and player score is $playerOne"
+			fi
+		#Snake
+			snake1=$( Snake $playerOne )
+			if(( $snake1 > 0 ))
+			then
+				playerOne=$(( $playerOne-$snake1 ))
+				echo "--> with help of snake low $snake1 the total score and player score is $playerOne "
+			fi
 		if(( $playerOne>100 ))
 		then
 			playerOne=$(( $playerOne-$play ))
