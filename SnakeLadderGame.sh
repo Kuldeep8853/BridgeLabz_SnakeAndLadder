915df9bb7b967c5f38e77fb9aaffadc186bc60e8
Kuldeep8853 <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-14 17:57:00 +0530

    Single player at start position 0

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,22 +1,40 @@
 #!/bin/bash/
-echo "Start the game Sanke and Ladder. The player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or asnake keeps doing till the winning spot 100 is achieved."
-playerOne=0
-count=0
-playGame(){
-	while(( playerOne<100 ))
+declare -A GameBoard[10,10]
+initializeGameBoard(){
+	number=100
+	for(( i=1;i<=10;i++ ))
 	do
-		play=$(( $(( $RANDOM%6 ))+1 ))
-		playerOne=$(( $playerOne+$play ))
-		count=$(( $count+1 ))
-		echo "$count time roll the die to get the $play"
-		if(( $playerOne>100 ))
+		if (( i%2 != 0 ))
 		then
-			playerOne=$(( $playerOne-$play ))
-			echo "Player score is $playerOne "
+			for(( j=1;j<=10;j++ ))
+			do
+				GameBoard[$i,$j]=$number
+				number=$(( $number-1 ))
+			done
 		else
-			echo "player score is $playerOne "
+			for(( j=10;j>0;j-- ))
+			do
+				GameBoard[$i,$j]=$number
+				number=$(( $number-1 ))
+			done
 		fi
+done
+}
+initializeGameBoard
+
+PrintGameBoard(){
+	echo "=============================================================="
+	for(( i=1;i<=10;i++ ))
+	do
+		for(( j=1;j<=10;j++ ))
+		do
+			value=$(printf "%03d" ${GameBoard[$i,$j]})
+			printf "| $value "
+		done
+		echo " |"
+		echo "=============================================================="
 	done
 }
-playGame
-echo "==> $count time  player is play the game "
+
+PrintGameBoard
+echo "Player Position is zero"
