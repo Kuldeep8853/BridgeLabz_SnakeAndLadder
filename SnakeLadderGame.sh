73e5f75e19ae803a9db5a4468c48d04dfde67fa9
Kuldeep8853 <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-14 18:02:19 +0530

    Welcome message Snake and Ladder game

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,2 +1,3 @@
 #!/bin/bash/
+
 echo "Start the game Sanke and Ladder. The player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or asnake keeps doing till the winning spot 100 is achieved."
